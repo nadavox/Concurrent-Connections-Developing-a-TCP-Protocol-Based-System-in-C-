ef536c7d254db52fc068741a89de9ba9d0feeffa
nadavox <nadavoknbarg@gmail.com>
2023-01-10 10:16:03 +0200

    left: put the input insde the vector list. in mission 1

--- Command_pattern/UploadCommand.cpp
+++ Command_pattern/UploadCommand.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void UploadCommand::execute()
 {
-    char buffer[4096];
+    char buffer[4096]; string received_data;
     string firstMessage = "Please upload your local train CSV file.\n";
     string Upload_Complete = "Upload complete.\n";
     //send function
@@ -18,6 +18,12 @@ void UploadCommand::execute()
     }
     memset(buffer, 0, sizeof(buffer));
     //receive function in a while until no more data
+    while (true) {
+        //TODO: if we get line by line from the client i want to put it inside the vector.
+        int bytes_received = recv(socket, buffer, strlen(buffer), 0);
+        if (bytes_received <= 0) break;
+        received_data.append(buffer, bytes_received);
+    }
     //save it in data structure classfiedVectorList.
     //send : Upload complete.
     strcpy(buffer, Upload_Complete.c_str());
@@ -35,6 +41,13 @@ void UploadCommand::execute()
     }
     memset(buffer, 0, sizeof(buffer));
     //receive function in a while until no more data
+    received_data = ""; // restart the string.
+    while (true) {
+        //TODO: if we get line by line from the client i want to put it inside the vector.
+        int bytes_received = recv(socket, buffer, strlen(buffer), 0);
+        if (bytes_received <= 0) break;
+        received_data.append(buffer, bytes_received);
+    }
     //save it in data structure notClassfiedVectorList.
     //send : Upload complete.
     strcpy(buffer, Upload_Complete.c_str());
